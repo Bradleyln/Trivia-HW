98d1239de45c5426d1d341d462b018f7cea9bc5f
unknown <bradleyln293@gmail.com>
2019-06-24 20:16:53 -0400

    changes

---
+++ Assets/Trivia.js/retry.js
@@ -0,0 +1,94 @@
+const qa = [
+    { question : "What city is Jay-Z from?",
+      answers :  [
+        "Toronto;",
+        "Brooklyn",
+        "Chicago",
+        "Los Angeles"
+      ],
+      correct : "Brooklyn"
+    },
+    { question : "What Kanye West last album title",
+      answers :  [
+        "Ye",
+        "My Beautiful Dark Twisted Fantasy",
+        "The Life of Pablo",
+        "Yeezus"
+      ],
+      correct : "Ye"
+    },
+    { question : "What does OVO stand for?",
+      answers :  [
+        "Do right and kill every thing",
+        "Octobers Very Own",
+        "The six",
+        "You Only Live Once"
+      ],
+      correct : "Octobers Very Own"
+    },
+    { question : "Who is Marshall Bruce Mathers",
+      answers :  [
+        "Machine Gun Kelly",
+        "Action Bronson",
+        "50 Cent",
+        "Eminem",
+      ],
+      correct : "Eminem"
+    },
+    { question : "Which one of these is NOT a producer?",
+      answers :  [
+        "Damon Dash",
+        "Timbaland",
+        "Snoop Dog",
+        "Dr Dre",
+      ],
+      correct : "Snoop Dog"
+    },
+    { question : "Which record label featured artists such as Dr. Dre, Tupac, and Snoop Dogg?",
+      answers :  [
+        "Death Row",
+        "Jr. Mafia",
+        "Rock-A-Fella",
+        "The Dungeon Family"
+      ],
+      correct : "Death Row"
+    },
+    { question : "At what age did the Notorious BIG pass",
+      answers :  [
+        "20",
+        "24",
+        "30",
+        "35"
+      ],
+      correct :
+        "24"
+    },
+    { question : "Which rapper was NOT a member of Wu-Tang Clan?",
+      answers : [
+        "ODB",
+        "Method Mans",
+        "U-God",
+        "Royce Da 5'9"
+      ],
+      correct : "Royce Da 5'9"
+    },
+    { question : "What was the name of Fresh Prince's DJ?",
+      answers : [
+        "King Coopa",
+        "Funk Master Flex",
+        "Jazzy Jeff",
+        "Envy"
+      ],
+      correct: "Jazzy Jeff"
+    },
+    { question : "Which Nas song did Jay-Z sample for his song Dead Presidents?",
+      answers : [
+        "Made You Look",
+        "If I Ruled The World",
+        "One Mic",
+        "The World is Yours"
+      ],
+      correct: "The World is Yours"
+    }
+  
+  ]
